3662f9cd24c010016dd548fb12fcc8bd16402ed4
Dusehu Théo <theo.dusehu@sfr.fr>
2023-11-14 00:42:34 +0100

    update port

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
 const app = express();
-const port = 8000;
+const port = 3000;
 
 const http = require('http');
 const server = http.createServer(app); // Use http.createServer to create a server
