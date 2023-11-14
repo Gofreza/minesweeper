293767c80caaa68f1d100ad2434eb7a8a8b55002
Dusehu Théo <theo.dusehu@sfr.fr>
2023-11-14 01:03:18 +0100

    update port

--- src/App.js
+++ src/App.js
@@ -58,6 +58,6 @@ app.use(authRoutes, testRoutes, adminRoutes);
 // *** Start server ***
 // ********************
 
-server.listen(port, '0.0.0.0', () => {
+server.listen(3000, '0.0.0.0', () => {
     console.log(`Server running at http://0.0.0.0:${port}/`);
 });
